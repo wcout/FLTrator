5d77000ab48c00ddb699302f49584d31e433125b
wcout <wcout@users.noreply.github.com>
2015-06-11 19:51:53 +0200

    +install option for fltrator-fullscreen.sh

--- fltrator-fullscreen.sh
+++ fltrator-fullscreen.sh
@@ -2,18 +2,28 @@
 
 module=xrandr_change_screen_res
 local=0
-if [ $(echo ${0} | cut -c1-2) = "./" ]
-then
+if [ $(echo ${0} | cut -c1-2) = "./" ]; then
 local=1
 prefix="./"
 fi
 
 ARGS=$1
 
-if [ $local ]
-then
+if [ $local ]; then
 echo "compiling $module module..."
 gcc -o $module $module.c -lX11 -lXrandr
+if [ $? = 0 ]; then
+if [ "$ARGS" = "--install" ]; then
+echo Installing ...
+DEST=~/bin/$(echo ${0} | rev | cut -c 4- | rev)
+echo cp "$0" "$DEST"
+cp "$0" "$DEST"
+chmod 0777 "$DEST"
+echo cp "$module" ~/bin/"$module"
+cp "$module" ~/bin/.
+exit 0
+fi
+fi
 fi
 
 # switch to 800x600
@@ -23,7 +33,7 @@ ORIG_MODE=$?
 echo "original screen mode was $ORIG_MODE"
 
 # start in fullscreen mode, no cursor
-"$prefix"fltrator $1 -f -c -e
+"$prefix"fltrator $ARGS -f -c -e
 
 # change back to original mode
 "$prefix"$module $ORIG_MODE
